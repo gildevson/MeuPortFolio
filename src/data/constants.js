2903a0850511a9f714cec8fadca5dd9d20fd55c9
gildevson <gilsonfonseca92@gmail.com>
2024-12-30 17:48:56 -0300

    Adicionando curriculo atualizado

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "Sou um profissional apaixonado por tecnologia, graduado em Análise e Desenvolvimento de Sistemas. Aqui, você encontrará meus projetos, tecnologias favoritas, formação acadêmica e certificações. Sempre busco me atualizar e aprimorar minhas habilidades para entregar soluções inovadoras. Explore meu portfólio e sinta-se à vontade para entrar em contato para perguntas. Estou sempre aberto a novas oportunidades e desafios.",
   github: "https://github.com/gildevson",
   resume:
-    "https://drive.google.com/file/d/1mGNCgBKZTDkiPPdqUwnEpf9M12l3xeca/view?usp=sharing",
+    "https://drive.google.com/file/d/11o7TMLXqHEEDtbFqA-4_yUSZ39h-Ugco/view",
   linkedin: "https://www.linkedin.com/in/gilson-fonseca-78b6b4138/",
   whatsapp: "",
   
