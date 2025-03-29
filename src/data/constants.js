85011aa04b1f5335b76491f2ddea993bce5b1587
Gilson Fonseca <gildevson@gmail.com>
2025-03-29 20:44:27 -0300

    Adicionando dados

--- src/data/constants.js
+++ src/data/constants.js
@@ -282,7 +282,7 @@ export const projects = [
     description:
       "O projeto foi desenvolvido com Node.js no backend e React no frontend, utilizando uma API RESTful para comunicação eficiente entre as camadas. O sistema foi pensado para resolver um problema comum em sistemas bancários: a dificuldade e o tempo perdido ao buscar informações em manuais bancários ou arquivos PDF.Esse problema surgiu porque, muitas vezes, as pessoas precisam cadastrar ocorrências bancárias ou buscar detalhes sobre layouts de cobrança, mas acabam gastando muito tempo navegando por documentos extensos e complexos. Para resolver isso, centralizei todas essas informações em uma interface simples e responsiva, facilitando o acesso.Com a integração ao banco de dados, é possível gerenciar ocorrências bancárias, acessar links diretos para layouts bancários e autenticar usuários com segurança. Além disso, o sistema organiza e apresenta os dados de forma clara, garantindo rapidez e eficiência no acesso às informações.Este projeto é ideal para empresas financeiras que buscam otimizar processos, reduzir o tempo de busca por informações e melhorar a organização de dados críticos, oferecendo uma solução prática e moderna.",
     image:
-      "https://raw.githubusercontent.com/gildevson/ONG/refs/heads/main/public/modelo.png",
+      "https://raw.githubusercontent.com/gildevson/OcorrenciasBancarias/refs/heads/main/src/components/Header/Layouts.png",
     tags: ["NodeJs","JavaScript","Bootstrap", "ReactJs", "CSS", "JSON"],
     category: "web app",
     github: "https://github.com/gildevson/OcorrenciasBancarias",
@@ -303,7 +303,7 @@ export const projects = [
     description:
       "Desenvolvi um website utilizando React.js com o objetivo de apresentar as ações sociais da Instituição Gerações, que realiza um trabalho fundamental no apoio a crianças e comunidades em situação de vulnerabilidade.O site foi pensado para ser moderno, leve e responsivo, permitindo que qualquer pessoa, em qualquer dispositivo, possa conhecer de forma clara e visual as atividades promovidas pela instituição.",
     image:
-      "https://github.com/gildevson/ONG",
+      "https://raw.githubusercontent.com/gildevson/ONG/refs/heads/main/public/modelo.png",
     tags: ["NodeJs","ReactJs", "CSS"],
     category: "web app",
     github: "https://github.com/gildevson/ONG",
